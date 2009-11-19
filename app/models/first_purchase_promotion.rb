bfd66abab9a1e357ae1afc1e665d94a6216c26df
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-11-19 14:54:25 +0100

    First purchase promotion

--- app/models/first_purchase_promotion.rb
+++ app/models/first_purchase_promotion.rb
@@ -1,16 +1,6 @@
-class FirstPurchasePromotion < Promotion
+class FirstPurchasePromotion < ProductPromotion
   # Checks if order is eligible for promotion.
   def eligible?(order)
-    eligible = true
-    eligible &&= Time.now >= start_at if start_at
-    eligible &&= Time.now <= end_at if end_at
-    eligible &&= user = order.user
-    eligible &&= (user.orders.checkout_complete.count == 0)
-    
-    return(eligible)
-  end
-
-  def default_calculator
-    self.calculator ||= Calculator::FlatPercentItemTotal.new
+    super && (user.orders.checkout_complete.count == 0)
   end
 end
